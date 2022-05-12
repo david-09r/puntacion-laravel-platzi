6699fb4395aee715def29eb66885df6a8ebceab8
David Rodriguez <davidchu1236@gmail.com>
2022-05-12 15:29:08 -0500

    Agregando todo lo relacionado a un CRUD API

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        $product = Product::create($request->all());
+
+        return $product;
+    }
+
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $product->update($request->all());
+
+        return response()->json(
+            [
+                "mensaje: " => "producto elimando"
+            ]
+        );
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return response()->json(
+            [
+                "mensaje: " => "producto elimando"
+            ]
+        );
+    }
+}
